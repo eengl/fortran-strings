1d74db73761d70473655a2d86ce6729f35f00f7e
Eric Engle <eengl@users.noreply.github.com>
2019-06-28 09:34:23 -0400

    Updated docs
    Updated docs

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,11 +1,13 @@
 var searchData=
 [
   ['str_5fcenter',['str_center',['../namespacestrings.html#a4200b0c5200b64c2c2a6ce8862e2af58',1,'strings']]],
-  ['str_5fcount',['str_count',['../namespacestrings.html#a1b755da0409a70ccc4c25c1de4e7e009',1,'strings']]],
+  ['str_5fcount',['str_count',['../namespacestrings.html#a4c059bd22bb1423242489b8b421f6119',1,'strings']]],
   ['str_5flower',['str_lower',['../namespacestrings.html#a9c44648f65a85e1e78b5e1c75ea67619',1,'strings']]],
   ['str_5freplace',['str_replace',['../namespacestrings.html#a2d557ee85358067ac58c78aeda61fefc',1,'strings']]],
   ['str_5freverse',['str_reverse',['../namespacestrings.html#a71f27e53beff6353ceead343b10761ec',1,'strings']]],
   ['str_5fsplit',['str_split',['../namespacestrings.html#ae0ef0af953180efc5cbda255e58e521c',1,'strings']]],
+  ['str_5fswapcase',['str_swapcase',['../namespacestrings.html#a5c3bdabb916c605add94b0490c0a300e',1,'strings']]],
+  ['str_5ftest',['str_test',['../namespacestrings.html#a0969d192486101d11d1bc69f68ee3d5b',1,'strings']]],
   ['str_5funiq',['str_uniq',['../namespacestrings.html#a8a6ee3280b88b0c63aa79b8095bd20ef',1,'strings']]],
   ['str_5fupper',['str_upper',['../namespacestrings.html#afd1c79d1ba7dfe3ca9c77cab4f936c99',1,'strings']]],
   ['str_5fzfill',['str_zfill',['../namespacestrings.html#af7c5fa6249abe3060899aab8ce88686e',1,'strings']]],
